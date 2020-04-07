a0728b571f9e325fe7838f22b3c17a292e38dfba
sdlee <leesungdae@skuniv.ac.kr>
2020-04-07 20:07:29 +0900

    Update Navigation Controller Update Navigation Controller engaged multi page

---
+++ helloworld/helloworld/DetailController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailController.swift
+//  helloworld
+//
+//  Created by 이성대 on 2020/04/07.
+//  Copyright © 2020 이성대. All rights reserved.
+//
+
+import UIKit
+
+class DetailController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 디테일 컨트롤러는 UI뷰 컨트롤러를 상속 받는다
+        // viewDidLoad()함수 오버라이딩
+    }
+}
--- helloworld/helloworld/ViewController.swift
+++ helloworld/helloworld/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Click Btn
+    @IBAction func Click_move(_ sender: Any) {
+        // story_board find controller, 'DetailController'
+        // '옵셔널 바인딩'  기법으로 선언
+        if let controller = self.storyboard?.instantiateViewController(withIdentifier: "DetailController")
+        {
+             // push controller = navi
+            self.navigationController?.pushViewController(controller, animated: true)
+            
+        }
+        
+        
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    // click event
-    @IBAction func Click_moveBtn(_ sender: Any) {
-        print("Click_move")
-    }
     
-
 }
 
