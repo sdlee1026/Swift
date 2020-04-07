28291046e6d751570316ab0f41966e2580917e72
sdlee <leesungdae@skuniv.ac.kr>
2020-04-07 18:47:26 +0900

    Update hellowolrd Update practice, about input buttonUI, LabelUI

---
+++ helloworld/helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by 이성대 on 2020/04/07.
+//  Copyright © 2020 이성대. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // click event
+    @IBAction func Click_moveBtn(_ sender: Any) {
+        print("Click_move")
+    }
+    
+
+}
+
