4105dc930bd8876bb037b8ea1a3e674e0753c444
sdlee <leesungdae@skuniv.ac.kr>
2020-09-18 17:24:48 +0900

    Update view

--- Nugugae/Nugugae/ViewController.swift
+++ Nugugae/Nugugae/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // id, pw 텍스트필드 입력 제한
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Login Start")
         // Do any additional setup after loading the view.
         id_textfield.delegate = self
         pw_textfield.delegate = self
@@ -22,21 +23,30 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var id_textfield: UITextField!
     @IBOutlet var pw_textfield: UITextField!
     
-    @IBAction func Login_btn(_ sender: Any) {
-        if (id_textfield.text!.count >= 0){
+    @IBAction func Login_btn(_ sender: UIButton) {
+        if (id_textfield.text!.count > 0) && (pw_textfield.text!.count > 0){
             print(id_textfield.text!)
+            print(pw_textfield.text!)
+            // 로그인 기능 수행
+            // server(db)요청
+                // 로그인 실패시 다시 이 화면, 아이디 비밀번호 확인 경고창
+                // 성공시 다음 화면으로 전환
+            
         }
         else{
-            print("x")
+            print("id, pw not input")
+            // id or pw 입력 없을 때 경고
         }
-        print(pw_textfield.text!)
     }
     
-    @IBAction func SignIn_btn(_ sender: Any) {
-        
+    @IBAction func SignIn_btn(_ sender: UIButton) {
+        print("SingIn Btn click")
+        // 회원가입 폼으로 이동
     }
     
-    @IBAction func SignIssue_btn(_ sender: Any) {
+    @IBAction func SignIssue_btn(_ sender: UIButton) {
+        print("SignIssue Btn click")
+        // 계정 이슈(아이디 or 비밀번호 이슈 폼 이동)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
