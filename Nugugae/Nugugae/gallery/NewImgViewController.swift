1ac64b71867828ac872f59958660331498cb4c16
sdlee <leesungdae@skuniv.ac.kr>
2020-10-15 01:12:54 +0900

    1005 finish

--- Nugugae/Nugugae/gallery/NewImgViewController.swift
+++ Nugugae/Nugugae/gallery/NewImgViewController.swift
@@ -31,6 +31,9 @@ class NewImgViewController: UIViewController, UITextFieldDelegate, CLLocationMan
     let date = DateFormatter()
     var kr:String = ""
     var img_date:String = ""
+    
+    var is_public:Bool = true
+    // 비공개 게시물 디폴트
 
     
     
@@ -45,12 +48,24 @@ class NewImgViewController: UIViewController, UITextFieldDelegate, CLLocationMan
     @IBOutlet weak var img_to_server_outlet: UIButton!
     @IBAction func img_to_server(_ sender: Any) {
         img_to_server_outlet.isEnabled = false
-        updateImgData(url: server_url+"/gallery/upload") { (ids) in
-            print(ids)
-            if ids.count != 0{
-                print("server to data, 비동기화 완료, dismiss")
-                self.dismiss(animated: true, completion: nil)
-                
+        if (self.is_public){
+            updateImgData(url: server_url+"/gallery/upload/private") { (ids) in
+                print(ids)
+                if ids.count != 0{
+                    print("server to data_private, 비동기화 완료, dismiss")
+                    self.dismiss(animated: true, completion: nil)
+                    
+                }
+            }
+        }// private 게시물
+        else{
+            updateImgData(url: server_url+"/gallery/upload/public") { (ids) in
+                print(ids)
+                if ids.count != 0{
+                    print("server to data_public, 비동기화 완료, dismiss")
+                    self.dismiss(animated: true, completion: nil)
+                    
+                }
             }
         }
     }
