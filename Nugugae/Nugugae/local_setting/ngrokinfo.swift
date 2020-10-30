8d237fee9fedc0289e3ff93ce63200c11edd67f8
sdlee <leesungdae@skuniv.ac.kr>
2020-10-30 23:56:57 +0900

    Update ngrok info

--- Nugugae/Nugugae/local_setting/ngrokinfo.swift
+++ Nugugae/Nugugae/local_setting/ngrokinfo.swift
@@ -9,7 +9,7 @@
 import Foundation
 class Server_url{
     static let sharedInstance = Server_url()
-    let server_url = "http://"+"672a57cca53d.jp"+".ngrok.io"
+    let server_url = "http://"+"5ff3cced6bbe.jp"+".ngrok.io"
     // ngork 재실행마다 url이 바뀜 해결해야함
 }
 
