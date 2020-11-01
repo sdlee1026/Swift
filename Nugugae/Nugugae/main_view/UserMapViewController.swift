2717d55f446bb2ea730ccedb76da58a892e35e44
sdlee <leesungdae@skuniv.ac.kr>
2020-11-01 18:40:59 +0900

    Update 1101

--- Nugugae/Nugugae/main_view/UserMapViewController.swift
+++ Nugugae/Nugugae/main_view/UserMapViewController.swift
@@ -23,6 +23,23 @@ class UserMapViewController:UIViewController, CLLocationManagerDelegate{
     // 위도와 경도
     var img_latitude: Double = -1
     var img_longitude: Double = -1
+    @IBAction func logout_btn(_ sender: Any) {
+        UserDefaults.standard.removeObject(forKey: "isLoggedIn")
+        UserDefaults.standard.removeObject(forKey: "userId")
+        print("로그아웃 버튼 클릭")
+        self.view.window?.rootViewController?.dismiss(animated: false, completion: {
+            guard let mainVC = UIStoryboard(name: "Main", bundle:nil).instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController else {
+                fatalError("Could not instantiate HomeVC!")
+            }
+            self.view.window?.rootViewController = mainVC
+            self.view.window?.makeKeyAndVisible()
+            
+        })//현재 윈도우에 root뷰 컨트롤러에 접근, 하위 뷰 삭제, main 뷰로 화면 전환
+        // self.view.window?.rootViewController = mainVC 메인 뷰컨트롤러로 새롭게 설정
+        // ScencDelegate
+    }
+    // 로그 아웃 버튼
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
